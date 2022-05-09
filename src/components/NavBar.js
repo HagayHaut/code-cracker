db7da33a98ff01f505ead6d672bdd18fff3f15b8
Hagay Haut <hagayhaut@gmail.com>
2022-05-08 21:34:31 -0400

    style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <div>
-        <h3>Help</h3>
-        <h3>Options</h3>
+    <div style={{ display:'flex', flexDirection:'row', justifyContent: 'space-between'}}>
+        <h3 style={{padding: '5px', border: '1 px solid black'}}>Help</h3>
+        <h3 style={{padding: '5px', border: '1 px solid black'}}>Options</h3>
     </div>
   )
 }
